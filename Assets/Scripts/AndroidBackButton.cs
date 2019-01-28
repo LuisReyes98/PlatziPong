395fe636ec39f781dbdfb7b6d8969234fd6f082c
LuisReyes98 <luisrogelio98@gmail.com>
2019-01-28 16:59:51 -0400

    git ignore added and android support

---
+++ Assets/Scripts/AndroidBackButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidBackButton : MonoBehaviour {
+
+	
+	
+	void FixedUpdate(){
+		if (Application.platform == RuntimePlatform.Android)
+		{
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+		}
+	}
+
+	
+}
